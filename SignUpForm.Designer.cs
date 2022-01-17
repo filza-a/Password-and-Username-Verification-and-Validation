1fad0fb0b1236b7169e0adc6359da77b4ab5b704
Filza Ahmad <64220634+filza-a@users.noreply.github.com>
2022-01-17 23:51:21 +0500

    Add files via upload

---
+++ SignUpForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace abc
+{
+    partial class SignUpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblUserNameInfo = new System.Windows.Forms.Label();
+            this.lblPasswordInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(41, 52);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(92, 15);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "Enter UserName";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(46, 104);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(87, 15);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Enter Password";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(161, 52);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(234, 23);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(161, 104);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(234, 23);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(161, 162);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(113, 27);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(275, 162);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(120, 27);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblUserNameInfo
+            // 
+            this.lblUserNameInfo.Location = new System.Drawing.Point(161, 78);
+            this.lblUserNameInfo.Name = "lblUserNameInfo";
+            this.lblUserNameInfo.Size = new System.Drawing.Size(234, 15);
+            this.lblUserNameInfo.TabIndex = 6;
+            // 
+            // lblPasswordInfo
+            // 
+            this.lblPasswordInfo.Location = new System.Drawing.Point(161, 130);
+            this.lblPasswordInfo.Name = "lblPasswordInfo";
+            this.lblPasswordInfo.Size = new System.Drawing.Size(234, 15);
+            this.lblPasswordInfo.TabIndex = 7;
+            // 
+            // SignUpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 218);
+            this.Controls.Add(this.lblPasswordInfo);
+            this.Controls.Add(this.lblUserNameInfo);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Name = "SignUpForm";
+            this.Text = "SignUpForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblUserNameInfo;
+        private System.Windows.Forms.Label lblPasswordInfo;
+    }
+}
---
+++ SignUpForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace abc
+{
+    public partial class SignUpForm : Form
+    {
+        public SignUpForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            validation();
+            verification();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.txtPassword.Text = null;
+            this.txtUserName.Text = null;
+        }
+
+        bool isOK;
+
+        //will validate the syntax of the username as well as the password
+        void validation()
+        {
+            bool isAlpha = false;
+            bool isNumeric = false;
+            isOK = true;
+
+            //making both the info labels null so each time on click of submit button, new message can appear in the label
+            lblUserNameInfo.Text = null;
+            lblPasswordInfo.Text = null;
+           
+            //restricting the length of username to be in the range 3-8
+            if (txtUserName.Text.Length < 3 || txtUserName.Text.Length > 8)
+            {
+                lblUserNameInfo.Text = "Length of username must be in range 3-8";
+                isOK = false;
+            }
+
+            //restricting the length of password to be greater than 6
+            if (txtPassword.Text.Length < 6)
+            {
+                lblPasswordInfo.Text = "The length of password must be atleast 6";
+                isOK = false;
+            }
+            //restricting alpha-numeric values only
+            else
+            {
+                foreach (char c in txtPassword.Text)
+                {
+                    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                    {
+                        isAlpha = true;
+                        continue;
+                    }
+                    else if (c >= '0' && c <= '9')
+                    {
+                        isNumeric = true;
+                        continue;
+                    }
+                    else
+                    {
+                        isAlpha = false;
+                        isNumeric = false;
+                        break;
+                    }
+                }
+                if ( ! (isAlpha && isNumeric) )
+                {
+                    lblPasswordInfo.Text = "Only alpha-numeric values allowed";
+                    isOK = false;
+                }
+            }
+
+        }
+
+
+        void verification()
+        {
+            if (isOK)
+            {
+                string[] allLines = File.ReadAllLines(@"C:\Users\filza\source\repos\abc\users.txt");
+
+                foreach (string s in allLines)
+                {
+                    if(txtUserName.Text == s.Split(';')[0])
+                    {
+                        MessageBox.Show("Username already exists");
+                        isOK = false;
+                    }
+                }
+
+                if (isOK)
+                {
+                    StreamWriter writer = File.AppendText(@"C:\Users\filza\source\repos\abc\users.txt");
+                    writer.WriteLine(txtUserName.Text + ";" + txtPassword.Text);
+                    writer.Close();
+                }
+
+            }
+            
+
+        }
+
+    }
+}
